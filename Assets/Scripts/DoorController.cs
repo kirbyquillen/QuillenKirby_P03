1d2043178114e0dfadb6e9fbac72b5c2540a499e
kirby <kirby.quillen@gmail.com>
2022-04-26 00:55:26 -0500

    Final Commit

---
+++ Assets/Scripts/DoorController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorController : MonoBehaviour
+{
+    Animator _doorAnim;
+
+    public void Open()
+    {
+        _doorAnim.SetBool("isOpening", true);
+    }
+
+    public void Close()
+    {
+        _doorAnim.SetBool("isOpening", false);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _doorAnim = this.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
